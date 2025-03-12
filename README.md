5d4b4d113f7e3870e57a1f665d57d5e12a928ebf
Priyarahul2002 <rahullvisha@gmail.com>
2025-03-12 11:09:16 +0000

    created a hello.js file

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # IT2234-p-
+
+This is some basic codes in web server technology
---
+++ hello.js
@@ -0,0 +1,58 @@
+console.log("Hello") //Prints Hello
+
+//arithmetic operations
+
+let num1 = 56
+let num2 = 65
+let sum = num1 + num2
+let sub = num2 - num1
+let mul = num1 * num2
+let div = num2 / num1			
+
+console.log(sum)	
+console.log(sub)
+console.log(mul)
+console.log(div)
+console.log()
+
+//Print the the numbers from  to 10
+console.log("Print numbers from 1 to 10")
+for(let x=1; x<=10; x++){
+	console.log(x)
+}
+
+console.log();
+
+//Printing odd numbers 
+console.log("Print only the odd numbers")
+for(let x=1; x<=10; x++){
+	if(x%2 == 1){
+		console.log(x)
+	}
+}
+
+console.log();
+
+//Printing the numbers in reverse order
+console.log("Print the numbers in reverse")
+for(let x=10; x>=1; x--){
+	if(x%2 == 1){
+		console.log(x)
+	}
+}
+
+console.log()
+
+console.log("Reverse the numbers in left & right 4321-5-9876")
+let mid = parseInt(9/2) + 1
+
+for(let y=mid-1; y>=1; y--){
+	console.log(y)
+}
+	
+console.log("-" + mid + "-")
+	
+for(let z=9; z>mid; z--){
+	console.log(z)
+}
+	
